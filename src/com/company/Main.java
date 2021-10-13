5db201dde65a8755c4596f6ab200d703d5b78ba8
Pranav Bhatia <bhatiapranav1996@gmail.com>
2021-10-13 16:18:51 -0400

    Read user

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,9 +2,10 @@ package com.company;
 
 import com.company.helper.DBHandler;
 
+import java.sql.ResultSet;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.PreparedStatement;
 
 public class Main {
     static private DBHandler dbHandler;
@@ -15,12 +16,28 @@ public class Main {
         dbHandler = new DBHandler();
         connection = dbHandler.getDbConnection();
 
+//        writeToDB();
+        readFromDB();
+    }
+
+
+    public static void readFromDB() throws SQLException {
+        String readQuery = "SELECT * from users";
+        PreparedStatement preparedStatement = connection.prepareStatement(readQuery);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        while (resultSet.next()) {
+            System.out.println("Name: " + resultSet.getString("firstname") + " " + resultSet.getString("lastname"));
+        }
+    }
+
+    public static void writeToDB() throws SQLException {
         String insertQuery = "INSERT INTO users(firstname,lastname,username,password) VALUES(?,?,?,?)";
 
         preparedStatement = connection.prepareStatement(insertQuery);
-        preparedStatement.setString(1, "Pranav");
-        preparedStatement.setString(2, "Bhatia");
-        preparedStatement.setString(3, "pb");
+        preparedStatement.setString(1, "Lionel");
+        preparedStatement.setString(2, "Messi");
+        preparedStatement.setString(3, "lm");
         preparedStatement.setString(4, "secret");
 
         preparedStatement.executeUpdate();
